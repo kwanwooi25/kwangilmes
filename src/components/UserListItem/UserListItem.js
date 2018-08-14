2a98f20b6a9fda98a3fe1f7d1d208e78b75823d9
unknown <kwanwoo.jeong@gmail.com>
2018-08-14 13:23:38 +0900

    updated user list item styles

--- src/components/UserListItem/UserListItem.js
+++ src/components/UserListItem/UserListItem.js
@@ -17,25 +17,31 @@ const UserListItem = ({
 		permission_id === 1 ? '최종관리자' : permission_id === 2 ? '관리자' : permission_id === 3 ? '사용자' : '';
 	return (
 		<li key={username} className="list-body__item">
-			<Grid container className="list-body__item-details">
-				<Grid item xs={12}>
+			<Grid container className="list-body__item-details user-list-item__details">
+				<Grid className="user-list-item__display_name" item xs={12}>
 					{display_name}
 				</Grid>
-				<Grid item xs={12}>
-					{user_department && <span>{user_department}</span>}
-				</Grid>
-				<Grid item xs={12}>
-					{user_position && <span>{user_position}</span>}
-				</Grid>
-				<Grid item xs={12}>
-					{user_phone && <span>{user_phone}</span>}
-				</Grid>
-				<Grid item xs={12}>
-					{user_permission && <span>{user_permission}</span>}
-				</Grid>
-				<Grid item xs={12}>
-					{user_memo && <span>{user_memo}</span>}
-				</Grid>
+				{user_phone && (
+					<Grid className="user-list-item__user_phone" item xs={12}>
+						<span>{user_phone}</span>
+					</Grid>
+				)}
+				{(user_department || user_position) && (
+					<Grid className="user-list-item__department-position" item xs={12}>
+						{user_department && <span>{user_department}</span>}
+						{user_position && <span>({user_position})</span>}
+					</Grid>
+				)}
+				{user_permission && (
+					<Grid className="user-list-item__user_permission" item xs={12}>
+						<span>사용권한: {user_permission}</span>
+					</Grid>
+				)}
+				{user_memo && (
+					<Grid className="user-list-item__user_memo" item xs={12}>
+						<span>{user_memo}</span>
+					</Grid>
+				)}
 			</Grid>
 			{current_user.can_write_users && (
 				<div className="list-body__item-buttons">
