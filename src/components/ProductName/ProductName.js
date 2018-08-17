bb5a2ce97e432ed2e040ce8538a75ab67ebd7c0d
unknown <kwanwoo.jeong@gmail.com>
2018-08-17 13:38:22 +0900

    add history to product detail view

--- src/components/ProductName/ProductName.js
+++ src/components/ProductName/ProductName.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import { connect } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Popover from '@material-ui/core/Popover';
@@ -7,177 +9,205 @@ import CustomModal from '../../components/CustomModal/CustomModal';
 import './ProductName.css';
 import { PRODUCT_DETAIL_SECTIONS } from '../../helpers/constants';
 
+const HOST = process.env.REACT_APP_API_HOST;
+
 class ProductName extends Component {
-  state = {
-    isDetailViewOpen: false,
-    anchorEl: null
-  };
+	state = {
+		isDetailViewOpen: false,
+		anchorEl: null
+	};
 
-  showDetailView = () => {
-    this.setState({ isDetailViewOpen: true });
-  };
+	showDetailView = () => {
+		this.setState({ isDetailViewOpen: true });
+	};
 
-  hideDetailView = () => {
-    this.setState({ isDetailViewOpen: false });
-  };
+	hideDetailView = () => {
+		this.setState({ isDetailViewOpen: false });
+	};
 
-  renderSections = data =>
-    PRODUCT_DETAIL_SECTIONS.map(({ title, fields }) => {
-      const hasInfo =
-        fields
-          .map(({ varName }) => !!data[varName])
-          .filter(value => value === true).length > 0;
+	renderSections = (data) =>
+		PRODUCT_DETAIL_SECTIONS.map(({ title, fields }) => {
+			const hasInfo = fields.map(({ varName }) => !!data[varName]).filter((value) => value === true).length > 0;
 
-      if (hasInfo) {
-        return (
-          <Grid item xs={12} md={6} key={title}>
-            <h2 className="detail-view__subtitle">{title}</h2>
-            {this.renderFields(fields, data)}
-          </Grid>
-        );
-      } else if (title === '작업내역') {
-        return (
-          <Grid item xs={12} md={6} key={title}>
-            <h2 className="detail-view__subtitle">{title}</h2>
-            {this.renderHistory(data)}
-          </Grid>
-        );
-      } else {
-        return undefined;
-      }
-    });
+			if (hasInfo) {
+				return (
+					<Grid item xs={12} md={6} key={title}>
+						<h2 className="detail-view__subtitle">{title}</h2>
+						{this.renderFields(fields, data)}
+					</Grid>
+				);
+			} else if (title === '작업내역') {
+				return (
+					<Grid item xs={12} md={6} key={title}>
+						<h2 className="detail-view__subtitle">{title}</h2>
+						{this.renderHistory(data)}
+					</Grid>
+				);
+			} else {
+				return undefined;
+			}
+		});
 
-  renderFields = (fields, data) =>
-    fields.map(({ varName, displayName }) => {
-      let value;
+	renderFields = (fields, data) =>
+		fields.map(({ varName, displayName }) => {
+			let value;
 
-      switch (varName) {
-        case 'product_size':
-          value = `${data.product_thick} x ${data.product_length} x ${
-            data.product_width
-          }`;
-          break;
-        case 'is_print':
-          value = data.is_print === 'is_print_true' ? '인쇄' : '무지';
-          break;
-        case 'ext_pretreat':
-          value =
-            data.ext_pretreat === 'single'
-              ? '단면'
-              : data.ext_pretreat === 'double'
-                ? '양면'
-                : '';
-          break;
-        case 'print_image_preview':
-          value = data.print_image_url;
-          break;
-        case 'ext_antistatic':
-        case 'cut_ultrasonic':
-        case 'cut_powder_pack':
-        case 'cut_is_punched':
-        case 'pack_deliver_all':
-          value = data[varName] ? 'Y' : 'N';
-          break;
-        default:
-          value = data[varName];
-      }
+			switch (varName) {
+				case 'product_size':
+					value = `${data.product_thick} x ${data.product_length} x ${data.product_width}`;
+					break;
+				case 'is_print':
+					value = data.is_print === 'is_print_true' ? '인쇄' : '무지';
+					break;
+				case 'ext_pretreat':
+					value = data.ext_pretreat === 'single' ? '단면' : data.ext_pretreat === 'double' ? '양면' : '';
+					break;
+				case 'print_image_preview':
+					value = data.print_image_url;
+					break;
+				case 'ext_antistatic':
+				case 'cut_ultrasonic':
+				case 'cut_powder_pack':
+				case 'cut_is_punched':
+				case 'pack_deliver_all':
+					value = data[varName] ? 'Y' : 'N';
+					break;
+				default:
+					value = data[varName];
+			}
 
-      if (value) {
-        if (varName === 'print_image_preview') {
-          return (
-            <div key={displayName} className="detail-view__row">
-              <span className="detail-view__name">{displayName}</span>
-              <span className="detail-view__value">
-                <Button
-                  fullWidth
-                  variant="contained"
-                  onClick={event => {
-                    this.setState({ anchorEl: event.target });
-                  }}
-                >
-                  도안보기
-                </Button>
-                <Popover
-                  className="detail-view__image-container"
-                  open={Boolean(this.state.anchorEl)}
-                  anchorEl={this.state.anchorEl}
-                  onClose={() => {
-                    this.setState({ anchorEl: null });
-                  }}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center'
-                  }}
-                  transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center'
-                  }}
-                >
-                  <img
-                    className="detail-view__image"
-                    src={data.print_image_url}
-                    alt={displayName}
-                    onClick={() => {
-                      this.setState({ anchorEl: null });
-                    }}
-                  />
-                </Popover>
-              </span>
-            </div>
-          );
-        }
+			if (value) {
+				if (varName === 'print_image_preview') {
+					return (
+						<div key={displayName} className="detail-view__row">
+							<span className="detail-view__name">{displayName}</span>
+							<span className="detail-view__value">
+								<Button
+									fullWidth
+									variant="contained"
+									onClick={(event) => {
+										this.setState({ anchorEl: event.target });
+									}}
+								>
+									도안보기
+								</Button>
+								<Popover
+									className="detail-view__image-container"
+									open={Boolean(this.state.anchorEl)}
+									anchorEl={this.state.anchorEl}
+									onClose={() => {
+										this.setState({ anchorEl: null });
+									}}
+									anchorOrigin={{
+										vertical: 'top',
+										horizontal: 'center'
+									}}
+									transformOrigin={{
+										vertical: 'bottom',
+										horizontal: 'center'
+									}}
+								>
+									<img
+										className="detail-view__image"
+										src={data.print_image_url}
+										alt={displayName}
+										onClick={() => {
+											this.setState({ anchorEl: null });
+										}}
+									/>
+								</Popover>
+							</span>
+						</div>
+					);
+				}
 
-        return (
-          <div key={displayName} className="detail-view__row">
-            <span className="detail-view__name">{displayName}</span>
-            <span className="detail-view__value">{value}</span>
-          </div>
-        );
-      } else return undefined;
-    });
+				return (
+					<div key={displayName} className="detail-view__row">
+						<span className="detail-view__name">{displayName}</span>
+						<span className="detail-view__value">{value}</span>
+					</div>
+				);
+			} else return undefined;
+		});
 
-  renderHistory = data => {
-    /*TODO: display history on product detail view */
-    console.log(data.old_history);
-    return <div className="detail-view__row">History</div>;
-  };
+	renderHistory = (data) => {
+		/*TODO: display history on product detail view */
+		// let history = [];
+		// const { id, old_history } = data;
+		// const token = this.props.auth.userToken;
 
-  render() {
-    const { product, searchTerm, className } = this.props;
+		// if (old_history) {
+		// 	history = old_history
+		// 		.split('매')
+		// 		.map((value) => {
+		// 			const array = value.split('수량:');
+		// 			if (array.includes('') === false) return array;
+		// 		})
+		// 		.filter((value) => value !== undefined)
+		// 		.map((data) => {
+		// 			return {
+		// 				date: data[0].trim(),
+		// 				quantity: Number(data[1].trim())
+		// 			};
+		// 		});
+		// }
 
-    const productName = highlight(product.product_name, searchTerm);
+		// return await fetch(`${HOST}/orders-by-product/${id}`, {
+		// 	headers: {
+		// 		'Content-Type': 'application/json',
+		// 		'x-access-token': token
+		// 	},
+		// 	method: 'get'
+		// })
+		// 	.then((response) => response.json())
+		// 	.then(({ data }) => {
+		// 		if (data) {
+		// 			data.forEach(({ ordered_at, order_quantity }) => {
+		// 				history.push({ date: moment(ordered_at).format('YYYY-MM-DD'), quantity: order_quantity });
+		// 			});
+		// 		}
 
-    return (
-      <div>
-        <a
-          className={`product-name ${className}`}
-          onClick={this.showDetailView.bind(this)}
-          dangerouslySetInnerHTML={{ __html: productName }}
-        />
-        {this.state.isDetailViewOpen && (
-          <CustomModal
-            className="product-detail-view"
-            title="품목 상세 정보"
-            open={this.state.isDetailViewOpen}
-            onClose={this.hideDetailView.bind(this)}
-            Buttons={
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={this.hideDetailView.bind(this)}
-              >
-                확인
-              </Button>
-            }
-          >
-            <div className="product-detail-view__contents">
-              {this.renderSections(product)}
-            </div>
-          </CustomModal>
-        )}
-      </div>
-    );
-  }
+		// 		console.log(history);
+
+		//   });
+
+		return <div className="detail-view__row">History</div>;
+	};
+
+	render() {
+		const { product, searchTerm, className } = this.props;
+
+		const productName = highlight(product.product_name, searchTerm);
+
+		return (
+			<div>
+				<span
+					className={`product-name ${className}`}
+					onClick={this.showDetailView.bind(this)}
+					dangerouslySetInnerHTML={{ __html: productName }}
+				/>
+				{this.state.isDetailViewOpen && (
+					<CustomModal
+						className="product-detail-view"
+						title="품목 상세 정보"
+						open={this.state.isDetailViewOpen}
+						onClose={this.hideDetailView.bind(this)}
+						Buttons={
+							<Button variant="contained" color="primary" onClick={this.hideDetailView.bind(this)}>
+								확인
+							</Button>
+						}
+					>
+						<div className="product-detail-view__contents">{this.renderSections(product)}</div>
+					</CustomModal>
+				)}
+			</div>
+		);
+	}
 }
 
-export default ProductName;
+const mapStateToProps = ({ auth }) => {
+	return { auth };
+};
+
+export default connect(mapStateToProps)(ProductName);
--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,375 +1,367 @@
 export const PRODUCT_FORM_INITIAL_STATE = {
-  isReviewOpen: false,
+	isReviewOpen: false,
 
-  // inputs
-  // basic info
-  account_id: '',
-  account_name: '',
-  product_name: '',
-  product_thick: '',
-  product_length: '',
-  product_width: '',
-  is_print: 'is_print_false',
+	// inputs
+	// basic info
+	account_id: '',
+	account_name: '',
+	product_name: '',
+	product_thick: '',
+	product_length: '',
+	product_width: '',
+	is_print: 'is_print_false',
 
-  // extrusion
-  ext_color: '',
-  ext_antistatic: false,
-  ext_pretreat: '', // 없음: '', '단면', '양면'
-  ext_memo: '',
+	// extrusion
+	ext_color: '',
+	ext_antistatic: false,
+	ext_pretreat: '', // 없음: '', '단면', '양면'
+	ext_memo: '',
 
-  // printing
-  print_front_color_count: 0,
-  print_front_color: '',
-  print_front_position: '',
-  print_back_color_count: 0,
-  print_back_color: '',
-  print_back_position: '',
-  print_image_file_name: '',
-  print_image_preview: '',
-  print_image_url: '',
-  print_memo: '',
+	// printing
+	print_front_color_count: 0,
+	print_front_color: '',
+	print_front_position: '',
+	print_back_color_count: 0,
+	print_back_color: '',
+	print_back_position: '',
+	print_image_file_name: '',
+	print_image_preview: '',
+	print_image_url: '',
+	print_memo: '',
 
-  // cutting
-  cut_position: '',
-  cut_ultrasonic: false,
-  cut_powder_pack: false,
-  cut_is_punched: false,
-  cut_punch_count: 0,
-  cut_punch_size: '',
-  cut_punch_position: '',
-  cut_memo: '',
+	// cutting
+	cut_position: '',
+	cut_ultrasonic: false,
+	cut_powder_pack: false,
+	cut_is_punched: false,
+	cut_punch_count: 0,
+	cut_punch_size: '',
+	cut_punch_position: '',
+	cut_memo: '',
 
-  // packaging
-  pack_material: '',
-  pack_unit: 0,
-  pack_deliver_all: false,
-  pack_memo: '',
+	// packaging
+	pack_material: '',
+	pack_unit: 0,
+	pack_deliver_all: false,
+	pack_memo: '',
 
-  // for manager
-  unit_price: 0,
-  product_memo: '',
+	// for manager
+	unit_price: 0,
+	product_memo: '',
 
-  // validation errors
-  account_name_error: '',
-  product_name_error: '',
-  product_thick_error: '',
-  product_length_error: '',
-  product_width_error: '',
-  ext_color_error: ''
+	// validation errors
+	account_name_error: '',
+	product_name_error: '',
+	product_thick_error: '',
+	product_length_error: '',
+	product_width_error: '',
+	ext_color_error: ''
 };
 
 export const PRODUCT_FORM_SECTIONS = [
-  {
-    title: '기본정보',
-    fields: [
-      { varName: 'account_name', displayName: '업체명', xs: 12, sm: 5, lg: 3 },
-      { varName: 'product_name', displayName: '품명', xs: 12, sm: 7, lg: 4 },
-      { varName: 'product_thick', displayName: '두께', xs: 4, sm: 3, lg: 1 },
-      {
-        varName: 'product_length',
-        displayName: '길이(압출)',
-        xs: 4,
-        sm: 3,
-        lg: 1
-      },
-      {
-        varName: 'product_width',
-        displayName: '너비(가공)',
-        xs: 4,
-        sm: 3,
-        lg: 1
-      },
-      {
-        type: 'radio',
-        varName: 'is_print',
-        options: [
-          { value: 'is_print_false', label: '무지' },
-          { value: 'is_print_true', label: '인쇄' }
-        ],
-        xs: 12,
-        sm: 3,
-        lg: 2
-      }
-    ]
-  },
-  {
-    title: '압출',
-    fields: [
-      { varName: 'ext_color', displayName: '압출색상', xs: 12, sm: 7, lg: 3 },
-      {
-        type: 'radio',
-        varName: 'ext_pretreat',
-        options: [
-          { value: '단면', label: '단면' },
-          { value: '양면', label: '양면' }
-        ],
-        xs: 7,
-        sm: 3,
-        lg: 2
-      },
-      {
-        type: 'checkbox',
-        varName: 'ext_antistatic',
-        displayName: '대전방지',
-        xs: 5,
-        sm: 2,
-        lg: 2
-      },
-      {
-        varName: 'ext_memo',
-        displayName: '압출메모',
-        xs: 12,
-        lg: 5,
-        multiline: true
-      }
-    ]
-  },
-  {
-    title: '인쇄',
-    fields: [
-      {
-        varName: 'print_image_url',
-        displayName: '기존 도안',
-        xs: 12
-      },
-      {
-        type: 'file',
-        varName: 'print_image_file_name',
-        displayName: '도안',
-        xs: 12
-      },
-      {
-        varName: 'print_front_color_count',
-        displayName: '전면도수',
-        xs: 4,
-        sm: 2
-      },
-      { varName: 'print_front_color', displayName: '전면색상', xs: 8, sm: 4 },
-      {
-        varName: 'print_front_position',
-        displayName: '전면인쇄위치',
-        xs: 12,
-        sm: 6
-      },
-      {
-        varName: 'print_back_color_count',
-        displayName: '후면도수',
-        xs: 4,
-        sm: 2
-      },
-      { varName: 'print_back_color', displayName: '후면색상', xs: 8, sm: 4 },
-      {
-        varName: 'print_back_position',
-        displayName: '후면인쇄위치',
-        xs: 12,
-        sm: 6
-      },
-      {
-        varName: 'print_memo',
-        displayName: '인쇄메모',
-        xs: 12,
-        multiline: true
-      }
-    ]
-  },
-  {
-    title: '가공',
-    fields: [
-      {
-        varName: 'cut_position',
-        displayName: '가공위치',
-        xs: 12,
-        sm: 7,
-        lg: 6
-      },
-      {
-        type: 'checkbox',
-        varName: 'cut_ultrasonic',
-        displayName: '초음파가공',
-        xs: 6,
-        sm: 3,
-        lg: 2
-      },
-      {
-        type: 'checkbox',
-        varName: 'cut_powder_pack',
-        displayName: '가루포장',
-        xs: 6,
-        sm: 2,
-        lg: 2
-      },
-      {
-        type: 'checkbox',
-        varName: 'cut_is_punched',
-        displayName: '바람구멍',
-        xs: 12,
-        sm: 2,
-        lg: 2
-      },
-      {
-        varName: 'cut_punch_count',
-        displayName: '바람구멍개수',
-        xs: 4,
-        sm: 2,
-        lg: 2
-      },
-      {
-        varName: 'cut_punch_size',
-        displayName: '바람구멍크기',
-        xs: 8,
-        sm: 3,
-        lg: 2
-      },
-      {
-        varName: 'cut_punch_position',
-        displayName: '바람구멍위치',
-        xs: 12,
-        sm: 5,
-        lg: 3
-      },
-      {
-        varName: 'cut_memo',
-        displayName: '가공메모',
-        xs: 12,
-        lg: 5,
-        multiline: true
-      }
-    ]
-  },
-  {
-    title: '포장',
-    fields: [
-      {
-        varName: 'pack_material',
-        displayName: '포장방법',
-        xs: 5,
-        sm: 6,
-        lg: 3
-      },
-      { varName: 'pack_unit', displayName: '포장단위', xs: 3, sm: 4, lg: 2 },
-      {
-        type: 'checkbox',
-        varName: 'pack_deliver_all',
-        displayName: '전량납',
-        xs: 4,
-        sm: 2,
-        lg: 2
-      },
-      {
-        varName: 'pack_memo',
-        displayName: '포장메모',
-        xs: 12,
-        lg: 5,
-        multiline: true
-      }
-    ]
-  },
-  {
-    title: '참고사항',
-    fields: [
-      { varName: 'unit_price', displayName: '단가', xs: 12, lg: 7 },
-      {
-        varName: 'product_memo',
-        displayName: '메모',
-        xs: 12,
-        lg: 5,
-        multiline: true
-      }
-    ]
-  }
+	{
+		title: '기본정보',
+		fields: [
+			{ varName: 'account_name', displayName: '업체명', xs: 12, sm: 5, lg: 3 },
+			{ varName: 'product_name', displayName: '품명', xs: 12, sm: 7, lg: 4 },
+			{ varName: 'product_thick', displayName: '두께', xs: 4, sm: 3, lg: 1 },
+			{
+				varName: 'product_length',
+				displayName: '길이(압출)',
+				xs: 4,
+				sm: 3,
+				lg: 1
+			},
+			{
+				varName: 'product_width',
+				displayName: '너비(가공)',
+				xs: 4,
+				sm: 3,
+				lg: 1
+			},
+			{
+				type: 'radio',
+				varName: 'is_print',
+				options: [ { value: 'is_print_false', label: '무지' }, { value: 'is_print_true', label: '인쇄' } ],
+				xs: 12,
+				sm: 3,
+				lg: 2
+			}
+		]
+	},
+	{
+		title: '압출',
+		fields: [
+			{ varName: 'ext_color', displayName: '압출색상', xs: 12, sm: 7, lg: 3 },
+			{
+				type: 'radio',
+				varName: 'ext_pretreat',
+				options: [ { value: '단면', label: '단면' }, { value: '양면', label: '양면' } ],
+				xs: 7,
+				sm: 3,
+				lg: 2
+			},
+			{
+				type: 'checkbox',
+				varName: 'ext_antistatic',
+				displayName: '대전방지',
+				xs: 5,
+				sm: 2,
+				lg: 2
+			},
+			{
+				varName: 'ext_memo',
+				displayName: '압출메모',
+				xs: 12,
+				lg: 5,
+				multiline: true
+			}
+		]
+	},
+	{
+		title: '인쇄',
+		fields: [
+			{
+				varName: 'print_image_url',
+				displayName: '기존 도안',
+				xs: 12
+			},
+			{
+				type: 'file',
+				varName: 'print_image_file_name',
+				displayName: '도안',
+				xs: 12
+			},
+			{
+				varName: 'print_front_color_count',
+				displayName: '전면도수',
+				xs: 4,
+				sm: 2
+			},
+			{ varName: 'print_front_color', displayName: '전면색상', xs: 8, sm: 4 },
+			{
+				varName: 'print_front_position',
+				displayName: '전면인쇄위치',
+				xs: 12,
+				sm: 6
+			},
+			{
+				varName: 'print_back_color_count',
+				displayName: '후면도수',
+				xs: 4,
+				sm: 2
+			},
+			{ varName: 'print_back_color', displayName: '후면색상', xs: 8, sm: 4 },
+			{
+				varName: 'print_back_position',
+				displayName: '후면인쇄위치',
+				xs: 12,
+				sm: 6
+			},
+			{
+				varName: 'print_memo',
+				displayName: '인쇄메모',
+				xs: 12,
+				multiline: true
+			}
+		]
+	},
+	{
+		title: '가공',
+		fields: [
+			{
+				varName: 'cut_position',
+				displayName: '가공위치',
+				xs: 12,
+				sm: 7,
+				lg: 6
+			},
+			{
+				type: 'checkbox',
+				varName: 'cut_ultrasonic',
+				displayName: '초음파가공',
+				xs: 6,
+				sm: 3,
+				lg: 2
+			},
+			{
+				type: 'checkbox',
+				varName: 'cut_powder_pack',
+				displayName: '가루포장',
+				xs: 6,
+				sm: 2,
+				lg: 2
+			},
+			{
+				type: 'checkbox',
+				varName: 'cut_is_punched',
+				displayName: '바람구멍',
+				xs: 12,
+				sm: 2,
+				lg: 2
+			},
+			{
+				varName: 'cut_punch_count',
+				displayName: '바람구멍개수',
+				xs: 4,
+				sm: 2,
+				lg: 2
+			},
+			{
+				varName: 'cut_punch_size',
+				displayName: '바람구멍크기',
+				xs: 8,
+				sm: 3,
+				lg: 2
+			},
+			{
+				varName: 'cut_punch_position',
+				displayName: '바람구멍위치',
+				xs: 12,
+				sm: 5,
+				lg: 3
+			},
+			{
+				varName: 'cut_memo',
+				displayName: '가공메모',
+				xs: 12,
+				lg: 5,
+				multiline: true
+			}
+		]
+	},
+	{
+		title: '포장',
+		fields: [
+			{
+				varName: 'pack_material',
+				displayName: '포장방법',
+				xs: 5,
+				sm: 6,
+				lg: 3
+			},
+			{ varName: 'pack_unit', displayName: '포장단위', xs: 3, sm: 4, lg: 2 },
+			{
+				type: 'checkbox',
+				varName: 'pack_deliver_all',
+				displayName: '전량납',
+				xs: 4,
+				sm: 2,
+				lg: 2
+			},
+			{
+				varName: 'pack_memo',
+				displayName: '포장메모',
+				xs: 12,
+				lg: 5,
+				multiline: true
+			}
+		]
+	},
+	{
+		title: '참고사항',
+		fields: [
+			{ varName: 'unit_price', displayName: '단가', xs: 12, lg: 7 },
+			{
+				varName: 'product_memo',
+				displayName: '메모',
+				xs: 12,
+				lg: 5,
+				multiline: true
+			}
+		]
+	}
 ];
 
 export const PRODUCT_FORM_REQUIRED = [
-  {
-    varName: 'account_name',
-    displayName: '업체명',
-    error: '업체명을 선택하세요.'
-  },
-  { varName: 'product_name', displayName: '품명', error: '품명을 입력하세요.' },
-  {
-    varName: 'product_thick',
-    displayName: '두께',
-    error: '두께를 입력하세요.'
-  },
-  {
-    varName: 'product_length',
-    displayName: '길이(압출)',
-    error: '길이(압출)를 입력하세요.'
-  },
-  {
-    varName: 'product_width',
-    displayName: '너비(가공)',
-    error: '너비(가공)를 입력하세요.'
-  },
-  {
-    varName: 'ext_color',
-    displayName: '원단색상',
-    error: '원단색상을 입력하세요.'
-  }
+	{
+		varName: 'account_name',
+		displayName: '업체명',
+		error: '업체명을 선택하세요.'
+	},
+	{ varName: 'product_name', displayName: '품명', error: '품명을 입력하세요.' },
+	{
+		varName: 'product_thick',
+		displayName: '두께',
+		error: '두께를 입력하세요.'
+	},
+	{
+		varName: 'product_length',
+		displayName: '길이(압출)',
+		error: '길이(압출)를 입력하세요.'
+	},
+	{
+		varName: 'product_width',
+		displayName: '너비(가공)',
+		error: '너비(가공)를 입력하세요.'
+	},
+	{
+		varName: 'ext_color',
+		displayName: '원단색상',
+		error: '원단색상을 입력하세요.'
+	}
 ];
 
 export const PRODUCT_DETAIL_SECTIONS = [
-  {
-    title: '기본정보',
-    fields: [
-      { varName: 'account_name', displayName: '업체명' },
-      { varName: 'product_name', displayName: '품명' },
-      { varName: 'product_size', displayName: '규격' },
-      { varName: 'is_print', displayName: '무지/인쇄' }
-    ]
-  },
-  {
-    title: '압출',
-    fields: [
-      { varName: 'ext_color', displayName: '압출색상' },
-      { varName: 'ext_pretreat', displayName: '인쇄전처리' },
-      { varName: 'ext_antistatic', displayName: '대전방지' },
-      { varName: 'ext_memo', displayName: '압출메모' }
-    ]
-  },
-  {
-    title: '인쇄',
-    fields: [
-      { varName: 'print_image_preview', displayName: '도안' },
-      { varName: 'print_front_color_count', displayName: '전면도수' },
-      { varName: 'print_front_color', displayName: '전면색상' },
-      { varName: 'print_front_position', displayName: '전면인쇄위치' },
-      { varName: 'print_back_color_count', displayName: '후면도수' },
-      { varName: 'print_back_color', displayName: '후면색상' },
-      { varName: 'print_back_position', displayName: '후면인쇄위치' },
-      { varName: 'print_memo', displayName: '인쇄메모' }
-    ]
-  },
-  {
-    title: '가공',
-    fields: [
-      { varName: 'cut_position', displayName: '가공위치' },
-      { varName: 'cut_ultrasonic', displayName: '초음파가공' },
-      { varName: 'cut_powder_pack', displayName: '가루포장' },
-      { varName: 'cut_is_punched', displayName: '바람구멍' },
-      { varName: 'cut_punch_count', displayName: '바람구멍개수' },
-      { varName: 'cut_punch_size', displayName: '바람구멍크기' },
-      { varName: 'cut_punch_position', displayName: '바람구멍위치' },
-      { varName: 'cut_memo', displayName: '가공메모' }
-    ]
-  },
-  {
-    title: '포장',
-    fields: [
-      { varName: 'pack_material', displayName: '포장방법' },
-      { varName: 'pack_unit', displayName: '포장단위' },
-      { varName: 'pack_deliver_all', displayName: '전량납' },
-      { varName: 'pack_memo', displayName: '포장메모' }
-    ]
-  },
-  {
-    title: '참고사항',
-    fields: [
-      { varName: 'unit_price', displayName: '단가' },
-      { varName: 'product_memo', displayName: '메모' }
-    ]
-  },
-  {
-    title: '작업내역'
-  }
+	{
+		title: '기본정보',
+		fields: [
+			{ varName: 'account_name', displayName: '업체명' },
+			{ varName: 'product_name', displayName: '품명' },
+			{ varName: 'product_size', displayName: '규격' },
+			{ varName: 'is_print', displayName: '무지/인쇄' }
+		]
+	},
+	{
+		title: '압출',
+		fields: [
+			{ varName: 'ext_color', displayName: '압출색상' },
+			{ varName: 'ext_pretreat', displayName: '인쇄전처리' },
+			{ varName: 'ext_antistatic', displayName: '대전방지' },
+			{ varName: 'ext_memo', displayName: '압출메모' }
+		]
+	},
+	{
+		title: '인쇄',
+		fields: [
+			{ varName: 'print_image_preview', displayName: '도안' },
+			{ varName: 'print_front_color_count', displayName: '전면도수' },
+			{ varName: 'print_front_color', displayName: '전면색상' },
+			{ varName: 'print_front_position', displayName: '전면인쇄위치' },
+			{ varName: 'print_back_color_count', displayName: '후면도수' },
+			{ varName: 'print_back_color', displayName: '후면색상' },
+			{ varName: 'print_back_position', displayName: '후면인쇄위치' },
+			{ varName: 'print_memo', displayName: '인쇄메모' }
+		]
+	},
+	{
+		title: '가공',
+		fields: [
+			{ varName: 'cut_position', displayName: '가공위치' },
+			{ varName: 'cut_ultrasonic', displayName: '초음파가공' },
+			{ varName: 'cut_powder_pack', displayName: '가루포장' },
+			{ varName: 'cut_is_punched', displayName: '바람구멍' },
+			{ varName: 'cut_punch_count', displayName: '바람구멍개수' },
+			{ varName: 'cut_punch_size', displayName: '바람구멍크기' },
+			{ varName: 'cut_punch_position', displayName: '바람구멍위치' },
+			{ varName: 'cut_memo', displayName: '가공메모' }
+		]
+	},
+	{
+		title: '포장',
+		fields: [
+			{ varName: 'pack_material', displayName: '포장방법' },
+			{ varName: 'pack_unit', displayName: '포장단위' },
+			{ varName: 'pack_deliver_all', displayName: '전량납' },
+			{ varName: 'pack_memo', displayName: '포장메모' }
+		]
+	},
+	{
+		title: '참고사항',
+		fields: [ { varName: 'unit_price', displayName: '단가' }, { varName: 'product_memo', displayName: '메모' } ]
+	},
+	{
+		title: '작업내역',
+		fields: []
+	}
 ];
